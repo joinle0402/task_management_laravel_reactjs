4a87a337334e424aea02ec109c0f39e14c384347
ngochophp <johnsmith2001it@gmail.com>
2026-07-13 16:50:39 +0700

    dọn api.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,12 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::prefix('v1')->group(function () {
     Route::prefix('auth')->group(function () {
